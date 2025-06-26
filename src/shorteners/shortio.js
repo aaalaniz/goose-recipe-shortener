61fffae8fe0e6ed99d2d2211605618651dfbe9a8
Aaron Alaniz <aaron@aalaniz.com>
2025-06-26 16:37:59 -0500

    Create or update shortio link

--- src/shorteners/shortio.js
+++ src/shorteners/shortio.js
@@ -11,16 +11,43 @@ class ShortioShortener extends GooseDeepLinkShortener {
 
   async shorten(url, path) {
     try {
-      const requestBody = {
-        domain: this.domain,
-        originalURL: url,
-        path: path
-      };
+      const getLinkUrl = `https://api.short.io/links/expand?domain=${encodeURIComponent(this.domain)}&path=${encodeURIComponent(path)}`;
+      let linkId = null;
+      let existingShortUrl = null;
+      try {
+        const getResponse = await this.httpClient.get(getLinkUrl, {
+          headers: { 'authorization': this.apiKey }
+        });
+        if (getResponse.data && getResponse.data.id) {
+          linkId = getResponse.data.id;
+          existingShortUrl = getResponse.data.shortURL;
+        }
+      } catch (err) {
+        if (err.message && !/404/.test(err.message)) {
+          throw err;
+        }
+      }
 
-      const response = await this.httpClient.post('https://api.short.io/links', requestBody, {
+      let postUrl;
+      let postBody;
+
+      if (linkId) {
+        postUrl = `https://api.short.io/links/${linkId}`;
+        postBody = { originalURL: url };
+      } else {
+        postUrl = 'https://api.short.io/links';
+        postBody = {
+          domain: this.domain,
+          originalURL: url,
+          path: path
+        };
+      }
+
+      const postResponse = await this.httpClient.post(postUrl, postBody, {
         headers: { 'authorization': this.apiKey }
       });
-      return response.data.shortURL;
+
+      return postResponse.data.shortURL;
     } catch (error) {
       throw new Error(`Short.io API error: ${error.response?.data?.message || error.message}`);
     }
